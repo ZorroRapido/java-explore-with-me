7331d8734198a835b18f80b6566be05fe20eb99e
i.s.melnikov <i.s.melnikov@tinkoff.ru>
2023-04-24 00:57:46 +0300

    fix: убрать лишний import из класса EndpointHit

--- stats/stats-dto/src/main/java/ru/practicum/EndpointHit.java
+++ stats/stats-dto/src/main/java/ru/practicum/EndpointHit.java
@@ -1,7 +1,6 @@
 package ru.practicum;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
